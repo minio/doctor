66d273e5cb7aeb597be0c3d0993d6cb842a044da
Deepa Mahalingam <Upheus1@Deepas-MacBook-Pro.local>
2016-06-27 12:59:36 -0700

    Added support for rendering tables from markdown and used prism.js to enable syntax highlighting.

--- app/controllers/hub_controller.rb
+++ app/controllers/hub_controller.rb
@@ -20,7 +20,13 @@ class HubController < ApplicationController
   def index
      
     file = open("https://raw.githubusercontent.com/deekoder/doctest/master/README.md").read 
-    @contents = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new).render(file)
+    
+    @contents = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new, :tables=>true,:fenced_code_blocks => true,
+          :no_intra_emphasis => true,
+          :autolink => true,
+          :strikethrough => true,
+          :lax_html_blocks => true,
+          :superscript => true).render(file)
     respond_to do |format|
        format.html { render :inline => @contents.html_safe , :layout=>'application'  }
     end   
