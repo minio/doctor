9b2b0a8c7e6f25f1dc0f46529b9b37516fa28758
Dee Koder <deekoder@users.noreply.github.com>
2016-06-27 13:26:21 -0700

    Added Support for  markdown tables & syntax highlighting (#14)
    * Added support for rendering tables from markdown and used prism.js to enable syntax highlighting.
    
    * Include a default dark css for syntax highlighting
    
    * Remove Minio specific files since Doctor is a general purpose doc CMS framework

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -30,7 +30,12 @@ class DocumentsController < ApplicationController
   def show
     @document = Document.find(params[:id])
     file = open(@document.link).read 
-    @contents = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new).render(file)
+    @contents = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new, :tables=>true,:fenced_code_blocks => true,
+          :no_intra_emphasis => true,
+          :autolink => true,
+          :strikethrough => true,
+          :lax_html_blocks => true,
+          :superscript => true).render(file)
     respond_to do |format|
        format.html { render :inline => @contents.html_safe , :layout=>'application'  }
     end  
