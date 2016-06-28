764a2859a3795352af5b3326699cff81308ac999
Dee Koder <deekoder@users.noreply.github.com>
2016-06-27 18:24:00 -0700

    Added User and Access Control (#16)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,32 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   before_filter :load_sidebar
+  before_filter :authorize
+
+  delegate :allow?, to: :current_permission
+  helper_method :allow?
+
+
+  private
+  
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
+  end
+  helper_method :current_user
+  
+  
+  
+  def current_permission
+    @current_permission ||= Permission.new(current_user)
+  end
+
+  def authorize
+    if !current_permission.allow?(params[:controller], params[:action])
+      redirect_to login_path, alert: "Not authorized."
+    end
+  end  
 
   def load_sidebar
     @categories = Category.order(:id)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620193220) do
+ActiveRecord::Schema.define(version: 20160627232809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 20160620193220) do
 
   add_index "documents", ["category_id", "created_at"], name: "index_documents_on_category_id_and_created_at", using: :btree
   add_index "documents", ["category_id"], name: "index_documents_on_category_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false
+  end
 
   add_foreign_key "documents", "categories"
 end
