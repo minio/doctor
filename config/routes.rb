fb36414ea177cb7e3181f67ae186afe814947874
Dee Koder <deekoder@users.noreply.github.com>
2016-06-29 19:20:46 -0700

    admin: Added an aggregating dashboard for admin user (#26)

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
-   
+  get  'dashboard', to: 'hub#dashboard', as: 'dashboard'
 
    
   root 'hub#index', as: 'hub'
