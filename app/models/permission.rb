56b0f3e50261bb0b70edf0cf916401d18fb307f0
Dee Koder <deekoder@users.noreply.github.com>
2016-07-20 15:03:46 -0700

    page access: Do not allow non admins to edit categories and documents. Fixes #83 (#86)

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -3,8 +3,8 @@ class Permission < Struct.new(:user)
     return true if controller == "sessions"
     return true if controller == "hub" && action.in?(%w[index])
     return true if controller == "users" && action != "index"
-    return true if controller == "categories" && action.in?(%w[index show])
-    return true if controller == "documents" && action.in?(%w[index show])
+    #return true if controller == "categories" && action.in?(%w[show])
+    return true if controller == "documents" && action.in?(%w[show])
   
     if user
       return true if controller == "categories" && action.in?(%w[index show edit new create])
