55337d43dad0c4b1a952beb87b84a1232ac43bad
Rushan <rushenn123@gmail.com>
2016-07-26 22:14:19 +0530

    Design and implement custom error pages  - 404 & 500

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  match '/404', :to => 'errors#not_found', :via => :all
+  match '/500', :to => 'errors#internal_server_error', :via => :all
+
+  get 'errors/not_found'
+
+  get 'errors/internal_server_error'
+
   resources :brands
   get 'sessions/new'
 
@@ -18,4 +25,6 @@ Rails.application.routes.draw do
   get  'dashboard', to: 'hub#dashboard', as: 'dashboard'
      
   root 'hub#index', as: 'hub'
+
+
 end
