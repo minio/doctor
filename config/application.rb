250b37169a59256de74a219c25de5e0e7002a622
Rushan <rushenn123@gmail.com>
2016-07-27 00:34:55 +0530

    Custom error pages and typography  (#135)
    * Refine fonts sizes to match original doc
    
    * Design and implement custom error pages
     - 404 & 500

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Doctor
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.exceptions_app = self.routes
   end
 end
