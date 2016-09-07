90b60b47843e9ba49f8c26a9e25008be70a0fff4
Dee Koder <deekoder@users.noreply.github.com>
2016-09-07 01:43:49 -0700

    bugfixes: Sets default values for configurable elements with color picker. (#197)
    * bugfixes: Move Pry & Fix #176
    
    * dashboard: Set default initial values for all settings & brand items in Doctor

--- db/migrate/20160812151324_add_menu_hover_background_color_to_brand.rb
+++ db/migrate/20160812151324_add_menu_hover_background_color_to_brand.rb
@@ -1,5 +1,5 @@
 class AddMenuHoverBackgroundColorToBrand < ActiveRecord::Migration
   def change
-    add_column :brands, :menu_hover_background_color, :string
+    add_column :brands, :menu_hover_background_color, :string,  :default => "#ededed"
   end
 end
--- db/migrate/20160812151909_add_menu_title_color_to_brand.rb
+++ db/migrate/20160812151909_add_menu_title_color_to_brand.rb
@@ -1,5 +1,5 @@
 class AddMenuTitleColorToBrand < ActiveRecord::Migration
   def change
-    add_column :brands, :menu_title_color, :string
+    add_column :brands, :menu_title_color, :string, :default => "#a2a2a2"
   end
 end
