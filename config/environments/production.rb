204c45600570d6bec43df1998be8f9538c63ac7e
Dee Koder <deekoder@users.noreply.github.com>
2016-09-13 17:11:40 -0700

    prodpicker : Put assets precompile instructions in production.rb (#210)
    * colopicker: Assets in vendor needs to be explicitly included in application.rb
    
    * colorpicker: Put assets precompile instructions in production.rb
    
    * Update application.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 end
