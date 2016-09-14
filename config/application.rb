ac14bf7fa84438e33a6f72b51f8df4711c297c3f
Deekoder <cmoviemaker@gmail.com>
2016-09-13 17:07:10 -0700

    colorpicker: Put assets precompile instructions in production.rb

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module Doctor
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.exceptions_app = self.routes
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    
   end
 end
