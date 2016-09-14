2a92f8810a313987fc8b794b08bb6b2123db9e1c
Dee Koder <deekoder@users.noreply.github.com>
2016-09-13 17:22:10 -0700

    assets: test if vendor images is included (#211)

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Doctor
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.exceptions_app = self.routes
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.paths << Rails.root.join("vendor", "assets", "images")
+   
   end
 end
