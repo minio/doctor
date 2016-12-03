f6bf1dea2754b1f6cc334d7e1ad655c6f8dd61ed
slava-nikulin <unicorninthecorn@gmail.com>
2016-12-03 12:59:34 +0500

    fix document name uniqueness, errors UI

--- app/models/document.rb
+++ app/models/document.rb
@@ -17,10 +17,14 @@
 class Document < ActiveRecord::Base
   belongs_to :category
   extend FriendlyId
-  friendly_id :name, use: :slugged
+  friendly_id :category_and_name, use: :slugged
 
   validates :name, :link, :presence => true
   validates :name, :length => { :minimum => 2 }
-  validates :name, :uniqueness => true
+  validates :name, uniqueness: {scope: :category_id}
   validates :link, :url => true
+
+  def category_and_name
+    [[category.title, :name]]
+  end
 end
--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class DocumentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "document name should be unique for each category" do
+    category1 = Category.create(title: 'cat1')
+    Document.create(category: category1, name: 'test', link: "http://ex.com")
+    new_doc = Document.new(category: category1, name: 'test', link: "http://ex.com")
+
+    assert_not new_doc.valid?
+    new_doc.category = Category.create(title: 'cat2')
+    assert new_doc.save
+  end
+
+  test "documents with same names should have correct slugs" do
+    new_doc = Document.create(category: Category.create(title: 'cat1'), name: 'test',
+                                                                       link: "http://ex.com")
+    new_doc1 = Document.create(category: Category.create(title: 'cat2'), name: 'test', 
+                                                                    link: "http://ex.com")
+    assert_equal new_doc.slug, "#{new_doc.category.title}-#{new_doc.name}"
+    assert_equal new_doc1.slug, "#{new_doc1.category.title}-#{new_doc1.name}"
+  end
 end
