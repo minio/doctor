92dca59bd8374838487945da91b418a0d150fac8
deekoder <cmoviemaker@gmail.com>
2018-04-04 16:55:41 -0700

    Upgraded rails to latest to update gems.

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module Doctor
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
     config.exceptions_app = self.routes
     config.assets.paths << Rails.root.join("vendor", "assets", "images")
    
